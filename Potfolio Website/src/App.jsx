f159ab574f195c4af86f25617747df3e8c0f0fa9
Neeraj <neeraj05042001@gmail.com>
2025-04-08 07:08:37 +0530

    first commit

---
+++ Potfolio Website/src/App.jsx
@@ -0,0 +1,16 @@
+import Hero from "./Components/Hero";
+import Navbar from "./Components/Navbar";
+
+const App = () => {
+  return (
+    <main className="max-w-7xl mx-auto">
+      
+      <Navbar/>
+      <Hero/>
+
+      
+    </main>
+  );
+};
+
+export default App;
